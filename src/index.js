91048d191c37409e512f70a0142203b22c931aa0
Bogdan Vilculescu <bogdan.vilculescu@gmail.com>
2020-06-14 12:36:31 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
     // res.render('');
