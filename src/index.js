5a1c51911cd820fa5693254c00b2be14b2960372
Bogdan Vilculescu <bogdan.vilculescu@gmail.com>
2020-06-14 12:31:01 +0300

    changed env file

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'));
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     // res.render('');
