888ed1c0f86588e87bc33dba16838dc32d9ad79e
Bogdan Vilculescu <bogdan.vilculescu@gmail.com>
2020-06-15 21:18:04 +0300

    added cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+
+const cors = require('cors');
+
 const fs= require('fs');
 
 app.set('view engine', 'ejs');
 
 // console.log(__dirname);
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
 
 const port = process.env.PORT || 8080;
